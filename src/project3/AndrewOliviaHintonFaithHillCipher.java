074895a3290d2cbc748786aa6248b51fad75a63b
Faith Hinton <hintonfj@dukes.jmu.edu>
2025-10-19 19:33:27 -0400

    start of file skeleton code

---
+++ src/project3/AndrewOliviaHintonFaithHillCipher.java
@@ -0,0 +1,13 @@
+class AndrewOliviaHintonFaithHillCipher{
+    public int[] encrypt(int plaintext[], int encryptionKey[][]){
+        return null;
+    }
+
+    public int[] decrypt(int ciphertext[], int decryptionKey[][]){
+        return null;
+    }
+    public static void main (String[] args) {
+
+    }
+
+}
